ea30979455a4b0f51a634521989b3595ccd9e21e
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-07-06 06:01:04 +0530

    added contact page

--- app/src/components/contact/index.tsx
+++ app/src/components/contact/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const Contact: React.FunctionComponent = () => {
-  return <div>Contact me</div>;
+  return (
+    <div className="contact-wrapper">
+      <h2>Get in touch with me</h2>
+      <p>
+        My inbox is always open. If you had any questions or just want to say
+        hello, I am hear.
+      </p>
+      <p>I will try to get back to you asap.</p>
+      <Button variant="outline-primary" size="lg">
+        Say hello
+      </Button>
+      <div className="about-contact">
+        <ul className="social-icons">
+          <li>
+            <a href="" className="social-icon">
+              <i className="fa fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="" className="social-icon">
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="" className="social-icon">
+              <i className="fa fa-facebook"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
