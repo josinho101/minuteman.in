2bbff8893d342bff212c814342353700f5da10a1
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-07-07 21:02:46 +0530

    Updates

---
+++ precache-manifest.02d8324f97f68177b52580b40fc78043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0aa4aa05d8b4660c5a35b03ba661ae",
+    "url": "/minuteman.in/index.html"
+  },
+  {
+    "revision": "2a77c9b837a42087c13c",
+    "url": "/minuteman.in/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2a77c9b837a42087c13c",
+    "url": "/minuteman.in/static/js/2.74527f72.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/minuteman.in/static/js/2.74527f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0d9f233c0a55e54089",
+    "url": "/minuteman.in/static/js/main.8fd5a9fe.chunk.js"
+  },
+  {
+    "revision": "8a8bd040dc087b1811cf",
+    "url": "/minuteman.in/static/js/runtime-main.9955b1a4.js"
+  }
+]);
