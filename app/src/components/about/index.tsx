1610069451c61b66371d94d1cee36be2a17ea9f1
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-07-09 05:48:27 +0530

    Added content for about page

--- app/src/components/about/index.tsx
+++ app/src/components/about/index.tsx
@@ -4,32 +4,73 @@ import SocialLinks from "../contact/sociallinks";
 const authorPng = require("../../assets/images/jj.jpg");
 
 const About: React.FunctionComponent = () => {
+  const getExperienceInYears = () => {
+    let start: any = new Date("2012-03-02");
+    let end: any = new Date();
+    let diffDate: any = new Date(end - start);
+
+    /*let exp = ` ${diffDate.toISOString().slice(0, 4) - 1970} years and ${
+      diffDate.getMonth() + 1
+    } months `;*/
+
+    let exp = ` ${diffDate.toISOString().slice(0, 4) - 1970}`;
+
+    return exp;
+  };
+
   return (
     <React.Fragment>
       <div className="col-md-8 blog-main">
         <div className="blog-post">
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            <span className="p-start">H</span>ello! I'm Joseph J Emmatty, from
+            India, with over <b>{getExperienceInYears()} years</b> of experience
+            in software design and development. While I'm a proficient
+            full-stack developer, my expertise includes building scalable
+            backend services (API) and front-end single page applications.
           </p>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Problems are fun to tackle, and I'm most interested in solving
+            architectural and performance-based problems, which will make
+            customers happy. With every problem solving I will get chance to
+            face new challenges and there by learning new things and improving
+            myself as a software engineer.
           </p>
+          <p>Here are some technologies I've been working recently:</p>
+          <b>
+            <ul>
+              <li>React JS, JavaScript/TypeScript</li>
+              <li>C#, .net core, ASP.Net MVC, Web API</li>
+            </ul>
+          </b>
+          <p>
+            Some of my personal projects added in 'projects' section and others
+            in my
+            <a href="https://github.com/josinho101" target="_blank">
+              {" "}
+              github{" "}
+            </a>
+            profile.
+          </p>
+          <p>
+            <em>My personal interests include:</em>
+          </p>
+          <ul>
+            <li>
+              <b>Machine learning</b>: Just started and working through online
+              videos and tutorials. I found it very exciting as it will make
+              machines learn from its experience.
+            </li>
+            <li>
+              <b>DIY projects</b>: It's fun to make things, I spend my free time
+              troubleshooting electrical and electronic gadgets and Arduino
+              board, it's fun working with it as I can program and control
+              hardware, which feels like my programs had come to life.
+            </li>
+            <li>
+              <b>Football</b>: A football lover and a big fan of FC Barcelona.
+            </li>
+          </ul>
           <div className="about-contact">
             <SocialLinks />
           </div>
